7316ec2cc69757e1d67f0ede6eb8c627191d6865
Thomas Parkison <trparky@toms-world.org>
2023-10-20 18:47:59 -0400

    Added code to kill processes.

--- Toms Updater/Module1.vb
+++ Toms Updater/Module1.vb
@@ -93,6 +93,10 @@ Module Module1
 
             Dim httpHelper As HttpHelper = CreateNewHTTPHelperObject()
 
+            Console.Write($"INFO: Killing process for {strProgramEXE}...")
+            SearchForProcessAndKillIt(strProgramEXE, False)
+            Console.WriteLine(" Done.")
+
             Using memoryStream As New MemoryStream()
                 Console.Write($"INFO: Downloading ZIP package file {Chr(34)}{strZIPFile}{Chr(34)}...")
                 If Not httpHelper.DownloadFile(strCombinedZIPFileURL, memoryStream, False) Then
@@ -141,6 +145,71 @@ Module Module1
             Process.GetCurrentProcess.Kill()
         End If
     End Sub
+
+    Private Function DoesProcessIDExist(PID As Integer, ByRef processObject As Process) As Boolean
+        Try
+            processObject = Process.GetProcessById(PID)
+            Return True
+        Catch ex As Exception
+            Return False
+        End Try
+    End Function
+
+    Private Sub KillProcess(processID As Integer)
+        KillProcessSubRoutine(processID)
+        Threading.Thread.Sleep(250) ' We're going to sleep to give the system some time to kill the process.
+        KillProcessSubRoutine(processID)
+        Threading.Thread.Sleep(250) ' We're going to sleep (again) to give the system some time to kill the process.
+    End Sub
+
+    Private Sub KillProcessSubRoutine(processID As Integer)
+        Dim processObject As Process = Nothing
+        If DoesProcessIDExist(processID, processObject) Then
+            Try
+                processObject.Kill()
+            Catch ex As Exception
+                ' Wow, it seems that even with double-checking if a process exists by it's PID number things can still go wrong.
+                ' So this Try-Catch block is here to trap any possible errors when trying to kill a process by it's PID number.
+            End Try
+        End If
+    End Sub
+
+    Private Sub SearchForProcessAndKillIt(strFileName As String, boolFullFilePathPassed As Boolean)
+        Dim processExecutablePath As String
+
+        For Each process As Process In Process.GetProcesses()
+            processExecutablePath = GetProcessExecutablePath(process.Id)
+
+            If Not String.IsNullOrWhiteSpace(processExecutablePath) Then
+                Try
+                    processExecutablePath = If(boolFullFilePathPassed, New IO.FileInfo(processExecutablePath).FullName, New IO.FileInfo(processExecutablePath).Name)
+                    If strFileName.Equals(processExecutablePath, StringComparison.OrdinalIgnoreCase) Then KillProcess(process.Id)
+                Catch ex As ArgumentException
+                End Try
+            End If
+        Next
+    End Sub
+
+    Private Function GetProcessExecutablePath(processID As Integer) As String
+        Try
+            Dim memoryBuffer As New Text.StringBuilder(1024)
+            Dim processHandle As IntPtr = NativeMethod.NativeMethods.OpenProcess(NativeMethod.ProcessAccessFlags.PROCESS_QUERY_LIMITED_INFORMATION, False, processID)
+
+            If Not processHandle.Equals(IntPtr.Zero) Then
+                Try
+                    Dim memoryBufferSize As Integer = memoryBuffer.Capacity
+                    If NativeMethod.NativeMethods.QueryFullProcessImageName(processHandle, 0, memoryBuffer, memoryBufferSize) Then Return memoryBuffer.ToString()
+                Finally
+                    NativeMethod.NativeMethods.CloseHandle(processHandle)
+                End Try
+            End If
+
+            NativeMethod.NativeMethods.CloseHandle(processHandle)
+            Return Nothing
+        Catch ex As Exception
+            Return Nothing
+        End Try
+    End Function
 
     Public Function CreateNewHTTPHelperObject() As HttpHelper
         Dim httpHelper As New HttpHelper With {
