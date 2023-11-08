f921c061833a5858a1abda7c3b96de3efb92d63a
Thomas Parkison <trparky@toms-world.org>
2023-11-08 00:23:36 -0500

    Added line mentioning closure of the ZIP file.

--- Toms Updater/Module1.vb
+++ Toms Updater/Module1.vb
@@ -4,7 +4,7 @@ Imports System.Security.Principal
 Imports System.Text.RegularExpressions
 
 Module Module1
-    Private Const strVersionString As String = "1.42"
+    Private Const strVersionString As String = "1.43"
     Private Const strMessageBoxTitleText As String = "Tom's Updater"
     Private Const strBaseURL As String = "https://www.toms-world.org/download/"
 
@@ -191,6 +191,9 @@ Module Module1
                             Console.WriteLine(" Done.")
                         Next
                     End Using
+
+                    ColoredConsoleLineWriter("INFO:")
+                    Console.WriteLine(" Closing ZIP file.")
                 Catch ex As InvalidDataException
                     memoryStream.Close()
                     memoryStream.Dispose()
